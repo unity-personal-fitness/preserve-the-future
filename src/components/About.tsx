b0f1f15c2277b222bcc57cec4a5d99c6e8387af1
unity-personal-fitness <unitypersonalfitness@gmail.com>
2026-03-17 12:33:50 -0500

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -65,6 +65,10 @@ export default function About({ setCurrentPage }: AboutProps) {
               
 
               {/* Our Values */}
+             <div className="w-full py-20">
+        <div className="max-w-6xl mx-auto px-6">
+<div className="prose prose-lg max-w-4xl mx-auto text-center space-y-8">
+  
               <div>
                 <h2 className="text-3xl font-bold text-gray-900 mb-8">Our Values</h2>
 
