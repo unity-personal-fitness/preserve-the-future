c76eeb584a337da3b778c75795177bac5d1c2ee6
unity-personal-fitness <unitypersonalfitness@gmail.com>
2026-03-18 11:08:58 -0500

    Updated Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -20,6 +20,7 @@ export default function Projects() {
         'https://i.ibb.co/qLF6tjNZ/image00004.jpg',
         'https://i.ibb.co/ymYftHcW/image00019.jpg',
         'https://i.ibb.co/NdhCnWfk/image00017.jpg',
+        'https://i.ibb.co/JjxshXcX/image00008.jpg',
       ],
       focus: ['Emergency Relief', 'Housing', 'Infrastructure'],
     },
